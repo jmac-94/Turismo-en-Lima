7e201d6f9fe23ddfe4f202e431986dd910bde0d6
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2021-10-29 00:06:00 -0500

    bd avance

--- backend/db.sql
+++ backend/db.sql
@@ -2,6 +2,15 @@ DROP DATABASE IF EXISTS limaturismo;
 CREATE DATABASE limaturismo;
 USE limaturismo;
 
+-- Lugares turisticos
+CREATE TABLE lugar (
+	lugar_id int NOT NULL AUTO_INCREMENT,
+	nombre varchar(50) NOT NULL,
+	descripcion varchar(300) NOT NULL,
+	ranking int NOT NULL,
+	PRIMARY KEY (lugar_id)
+);
+
 -- Comentarios
 CREATE TABLE comentarios (
 	comment_id int NOT NULL AUTO_INCREMENT,
@@ -13,37 +22,32 @@ CREATE TABLE comentarios (
     PRIMARY KEY (comment_id)
 );
 
---Respuestas
-CREATE TABLE respuestas (
-	respuesta_id int NOT NULL,
-	respuesta varchar(255) NOT NULL,
-	PRIMARY KEY (respuesta_id)
+CREATE TABLE trivia (
+	id int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
 );
 
 --Preguntas
 CREATE TABLE preguntas (
-	pregunta_id int NOT NULL,
-	pregunta varchar(255) NOT NULL,
-	correcta int NOT NULL,
-	PRIMARY KEY (pregunta_id),
-	FOREIGN KEY (correcta) REFERENCES respuestas (respuesta_id)
-);
-
-CREATE TABLE trivia (
+	id int NOT NULL AUTO_INCREMENT,
 	trivia_id int NOT NULL,
-	PRIMARY KEY (trivia_id)
+	pregunta varchar(255) NOT NULL,
+	FOREIGN KEY (trivia_id) REFERENCES trivia (id),
+	PRIMARY KEY (id, trivia_id)
 );
 
--- Lugares turisticos
-CREATE TABLE lugar (
-	lugar_id int NOT NULL AUTO_INCREMENT,
-	nombre varchar(50) NOT NULL,
-	descripcion varchar(300) NOT NULL,
-	ranking int NOT NULL,
-	trivia_id int,
-	PRIMARY KEY (lugar_id),
-	FOREIGN KEY (trivia_id) REFERENCES trivia (trivia_id)
+--Respuestas
+CREATE TABLE respuestas (
+	id int NOT NULL AUTO_INCREMENT,
+	trivia_id int NOT NULL,
+	pregunta_id int NOT NULL,
+	respuesta varchar(255) NOT NULL,
+	correcta BOOLEAN NOT NULL,
+	FOREIGN KEY (pregunta_id, trivia_id) REFERENCES preguntas(id, trivia_id),
+	PRIMARY KEY (id, trivia_id, pregunta_id)
 );
+
+
 
 -- Tabla de imagenes de lugares turisticos
 
@@ -53,16 +57,4 @@ INSERT INTO comentarios(name_user,lname_user,coments_user,points_obtained,time_c
 	('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
 	('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
 	('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
-
-INSERT INTO T_Comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
-VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
-('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
-('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
-('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
-
-INSERT INTO T_Comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
-VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
-('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
-('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
-('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
 
