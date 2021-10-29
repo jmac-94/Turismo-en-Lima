a96c0c8768f378f56f0284098b044d04c9d72072
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2021-10-29 08:03:27 -0500

    inserts a la base de datos hechos

--- backend/db.sql
+++ backend/db.sql
@@ -3,10 +3,10 @@ CREATE DATABASE limaturismo;
 USE limaturismo;
 
 -- Lugares turisticos
-CREATE TABLE lugar (
+CREATE TABLE lugares (
 	id int NOT NULL AUTO_INCREMENT,
 	nombre varchar(50) NOT NULL,
-	descripcion varchar(300) NOT NULL,
+	descripcion varchar(1000) NOT NULL,
 	ranking int NOT NULL,
 	PRIMARY KEY (id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE comentarios (
 	points_obtained int NOT NULL,
 	time_comment DATETIME NOT NULL,
     PRIMARY KEY (id, lugar_id),
-	FOREIGN KEY (lugar_id) REFERENCES lugar (id)
+	FOREIGN KEY (lugar_id) REFERENCES lugares (id)
 );
 
 -- Trivias
@@ -29,7 +29,7 @@ CREATE TABLE trivias (
 	id int NOT NULL AUTO_INCREMENT,
 	lugar_id int NOT NULL,
 	PRIMARY KEY (id, lugar_id),
-	FOREIGN KEY (lugar_id) REFERENCES lugar (id)
+	FOREIGN KEY (lugar_id) REFERENCES lugares (id)
 );
 
 -- Preguntas
@@ -54,15 +54,29 @@ CREATE TABLE respuestas (
 	FOREIGN KEY (pregunta_id, lugar_id, trivia_id) REFERENCES preguntas(id, lugar_id, trivia_id)
 );
 
--- Tabla de imagenes de lugares turisticos
 
-/*
-INSERT INTO comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
-	VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
-	('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
-	('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
-	('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
-*/
+
+INSERT INTO lugares (nombre, descripcion, ranking)
+	VALUES ('Museo Larco', 'MMMMMM', 10),
+	('Islas Palomino', 'IIIIII', 9),
+	('Plaza de Armas de Lima', 'PPPPP', 9);
+
+INSERT INTO comentarios(lugar_id, name_user, lname_user,coments_user, points_obtained, time_comment) 
+	VALUES (1, 'Wanly', 'Obregon', 'Me parece muy buena la interfaz', 10, now()),
+	(1, 'Luis', 'Izaguirre', 'Interesante pero le falta mas animacion', 8, now()),
+	(1, 'Valerie', 'Ore', 'Estuvo muy lindo woow!!', 10, now()),
+	(1, 'Pedro', 'Rodriguez', 'Hicieron lo que pudieron xd', 2, now());
+
+INSERT INTO trivias (lugar_id)
+	VALUES (1);
+
+INSERT INTO preguntas (lugar_id, trivia_id, pregunta)
+	VALUES (1, 1, '¿Cuál es el nombre del lugar turistico?');
+
+INSERT INTO respuestas (lugar_id, trivia_id, pregunta_id, respuesta, correcta)
+	VALUES (1, 1, 1, 'Museo Larco', true),
+	(1, 1, 1, 'Wikipedia', false);
+
 
 
 -- INSTRUCTIONS TO CHECK IF TABLES HAVE THE CORRECT CONSTRAINTS
