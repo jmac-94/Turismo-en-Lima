8a62ba7a682faa4e7d27d55cd14035b203c9d80f
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2021-10-28 23:21:49 -0500

    bd avance 2

--- backend/db.sql
+++ backend/db.sql
@@ -2,7 +2,8 @@ DROP DATABASE IF EXISTS limaturismo;
 CREATE DATABASE limaturismo;
 USE limaturismo;
 
-CREATE TABLE T_Comentarios(
+-- Comentarios
+CREATE TABLE comentarios (
 	comment_id int NOT NULL AUTO_INCREMENT,
 	name_user varchar(50) NOT NULL,
 	lname_user varchar(50) NOT NULL,
@@ -12,7 +13,46 @@ CREATE TABLE T_Comentarios(
     PRIMARY KEY (comment_id)
 );
 
-CREATE TABLE 
+--Respuestas
+CREATE TABLE respuestas (
+	respuesta_id int NOT NULL,
+	respuesta varchar(255) NOT NULL,
+	PRIMARY KEY (respuesta_id)
+);
+
+--Preguntas
+CREATE TABLE preguntas (
+	pregunta_id int NOT NULL,
+	pregunta varchar(255) NOT NULL,
+	correcta int NOT NULL,
+	PRIMARY KEY (pregunta_id),
+	FOREIGN KEY (correcta) REFERENCES respuestas (respuesta_id)
+);
+
+CREATE TABLE trivia (
+	trivia_id int NOT NULL,
+	PRIMARY KEY (trivia_id)
+);
+
+-- Lugares turisticos
+CREATE TABLE lugar (
+	lugar_id int NOT NULL AUTO_INCREMENT,
+	nombre varchar(50) NOT NULL,
+	descripcion varchar(300) NOT NULL,
+	ranking int NOT NULL,
+	trivia_id int,
+	PRIMARY KEY (lugar_id),
+	FOREIGN KEY (trivia_id) REFERENCES trivia (trivia_id)
+);
+
+-- Tabla de imagenes de lugares turisticos
+
+-- 
+INSERT INTO comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
+	VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
+	('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
+	('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
+	('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
 
 INSERT INTO T_Comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
 VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
@@ -20,5 +60,9 @@ VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
 ('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
 ('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
 
-SELECT name_user,lname_user,coments_user,points_obtained,time_comment
-FROM T_Comentarios;
+INSERT INTO T_Comentarios(name_user,lname_user,coments_user,points_obtained,time_comment) 
+VALUES ('Wanly','Obregon','Me parece muy buena la interfaz',10,now()),
+('Luis','Izaguirre','Interesante pero le falta mas animacion',8,now()),
+('Valerie','Ore','Estuvo muy lindo woow!!',10,now()),
+('Pedro','Rodriguez','Hicieron lo que pudieron xd',2,now());
+
