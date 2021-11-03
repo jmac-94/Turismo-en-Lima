893d5a8be10675b92921847340969cbc667268f4
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2021-11-03 11:59:55 -0500

    directorios creados en frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{ P2Component } from '../app/p2/p2.component';
+import{ P1Component } from '../app/p1/p1.component';
 
 const routes: Routes = [
+  {path:'',component: P1Component},
   {path:'P2',component: P2Component}
 ];
 
