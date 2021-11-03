ad23970699d14010cf940ed801b2e6d125ed757a
LuisIZ <luis.izaguirre@utec.edu.pe>
2021-11-03 11:41:52 -0500

    hola

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { CommentsTableComponent } from './comments-table/comments-table.component';
 import { P2Component } from './p2/p2.component';
 import { AppRoutingModule } from './app-routing.module';
+import { P1Component } from './p1/p1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CommentsTableComponent,
-    P2Component
+    P2Component,
+    P1Component
   ],
   imports: [
     BrowserModule,
