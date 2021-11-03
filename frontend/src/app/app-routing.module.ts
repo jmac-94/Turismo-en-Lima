c388c2bffd4ad1379070e94e1b8291fb90113ace
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2021-11-03 17:44:06 -0500

    -app-routing.module correcto -app.module.ts correcto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{ P2Component } from '../app/p2/p2.component';
 import{ P1Component } from '../app/p1/p1.component';
+import { P3Component } from './p3/p3.component';
 
 const routes: Routes = [
   {path:'',component: P1Component},
-  {path:'P2',component: P2Component}
+  {path:'P2',component: P2Component},
+  {path:'P2/vistas',component: P3Component}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
 import { CommentsTableComponent } from './comments-table/comments-table.component';
-import { P2Component } from './p2/p2.component';
-import { AppRoutingModule } from './app-routing.module';
 import { P1Component } from './p1/p1.component';
+import { P2Component } from './p2/p2.component';
 import { P3Component } from './p3/p3.component';
 import { P3L1Component } from './p3-l1/p3-l1.component';
 import { P3L2Component } from './p3-l2/p3-l2.component';
